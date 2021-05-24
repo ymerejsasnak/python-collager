2dc4ed9d91b5d4b329fc48427b81fe88e01e0085
Jeremy Kansas <ymerejsasnak@gmail.com>
2021-05-24 14:52:54 -0400

    refactored collage functions into collage class

--- collager.py
+++ collager.py
@@ -41,113 +41,105 @@ def get_wav_paths(parent_dir='D:/Samples', sub_dirs=[]):
 
 
   
-def choose_samples(paths, num_to_load=2):
-    """
-    Choose a number of samples from a given list of sample paths
-    Also displays a message on load or on load failure for each sound
-    
-    **NOTE THAT THERE IS NO CHECK FOR SITUATIONS WHERE NOT ENOUGH FILES CAN BE LOADED!**
-    
-    Arguments:
-    paths -- list of full pathnames of .wav files (typically from get_wav_paths)
-    num_to_load -- number of files to load, defaults to 10
-    
-    Returns:
-    a list of samples (pydub AudioSegments)
-    """
 
-    samples = []
-
-    while len(samples) < num_to_load:
-
-        path_choice = str(choice(paths))
-
-        try:
-            sample = AudioSegment.from_file(path_choice, format='wav')
-            samples.append(sample)
-            print("loaded " + path_choice)
-
-        except Exception:
-            print("cannot load " + path_choice)
-
-    return samples
 
 
 
 
 
 class CollageMaker:
-
-    self.settings = {
-    
-    }
-    
-    self.settings[output_length=
-    
+       
     def __init__(self):
-        pass
+        self.settings = {
+            'number_to_load': 2,
+            'output_length': 1000,
+            'slice_length': 100,
+            'iterations': 10,
+        }
     
     
-    def set_samples(self, samples):
-        this.samples = samples
+    def set_paths(self, paths):
+        self.paths = paths
     
     
-    def collage_settings(settings_dict):
-        for k, v in settings_dict:
-            self.k = self.kv
+    def choose_samples(self):
+        """
+       
+        """
+
+        self.samples = []
+
+        while len(self.samples) < self.settings['number_to_load']:
+
+            path_choice = str(choice(self.paths))
+
+            try:
+                sample = AudioSegment.from_file(path_choice, format='wav')
+                self.samples.append(sample)
+                print("loaded " + path_choice)
+
+            except Exception:
+                print("cannot load " + path_choice)
+
+    
+    def update_settings(self, new_settings):
+        for k, v in new_settings:
+            self.settings[k] = v or self.settings[k]
     
     
     
 
-def create_collage(samples, output_length=1000, slice_length=100, iterations=10):
-    """
-    Create a collage by providing a list of AudioSegments, with various optional settings.
-    
-    
-    Arguments:
-    output_length --
-    slice_count --
-    
-    
-    """
-    
-    collage = AudioSegment.silent(output_length, frame_rate=44100)
+    def create_collage(self):
+        """
+             
+        """
+        
+        self.collage = AudioSegment.silent(self.settings['output_length'], frame_rate=44100)
 
-    db_adjust = 20 * log(1/iterations, 10)    # really don't need to reduce gain by this much...maybe base more on rms????
+        db_adjust = 20 * log(1/self.settings['iterations'], 10)    # really don't need to reduce gain by this much...maybe base more on rms????
 
-    for i in range(iterations):
+        for i in range(self.settings['iterations']):
 
-        sample = choice(samples)
+            sample = choice(self.samples)
 
-        if len(sample) > slice_length:
-            start = randint(0, len(sample) - slice_length)
-            sample = sample[start : start + slice_length]
-            
-        sample = sample.apply_gain(db_adjust) 
-        collage = collage.overlay(sample, position=randint(0, output_length-slice_length))
-           
-    return collage
+            if len(sample) > self.settings['slice_length']:
+                start = randint(0, len(sample) - self.settings['slice_length'])
+                sample = sample[start : start + self.settings['slice_length']]
+                
+            sample = sample.apply_gain(db_adjust) 
+            self.collage = self.collage.overlay(sample, position=randint(0, self.settings['output_length']-self.settings['slice_length']))
+               
+        self.collage = normalize(self.collage)
+        
+        
+    
+    def export_collage(self):
+        self.collage.export(datetime.now().strftime('%m-%d-%Y %H.%M.%S.wav'), format='wav')
     
     
     
     
+
+
 
 
 # ideas - min/max length, recurring sounds, repetitions, xfade, changes(pitch/vol/pan/etc), fx(filter, others), overlay, reverse, pieces of sounds
 #sync to a bpm?  rhythmic and or melodic algorithms?   note/beat detection...etc
 #normalize slices before adding them
 
-'''
-paths = get_wav_paths()
-samples = choose_samples(paths)
-collage = create_collage(samples)
-'''
 
-paths = get_wav_paths(sub_dirs=['FROM TABLET'])
-samples = choose_samples(paths, num_to_load=20)
-collage = create_collage(samples, output_length=20000, iterations=1000, slice_length=50)
 
-collage = normalize(collage)
 
-collage.export(datetime.now().strftime("%m-%d-%Y %H.%M.%S.wav"), format="wav")   
+c = CollageMaker()
+c.set_paths(get_wav_paths())
+c.choose_samples()
+c.create_collage()
+c.export_collage()
+
+
+
+
+
+
+
 
