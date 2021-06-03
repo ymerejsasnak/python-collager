b9b73d03b1746afded3e16f231e4ef480531fb61
Jeremy Kansas <ymerejsasnak@gmail.com>
2021-06-03 12:33:58 -0400

    settings class changed to use namedtuples

--- collagemaker/settings.py
+++ collagemaker/settings.py
@@ -1,35 +1,53 @@
+from collections import namedtuple
+
+
 class Settings:
 
     def __init__(self):
+        self.text = namedtuple('val', 'val2')
 
-        self.collage = {
-            'parent_dir': 'D:/Samples',
-            'sub_dirs': [],
-            'main_pool_size': 30,
-            'sections': ('a', 'b'),
-            'structure': ('a', 'b', 'a', 'b', 'a'),
-            'overlap': 0.1,  # % of section length
-        }
+        Collage = namedtuple('Collage',
+                             ('parent_dir', 'sub_dirs', 'main_pool_size', 'sections', 'structure', 'overlap'))
 
-        self.section = {
-            'length': 30,  # seconds
-            'motif_count': 10,
-            'samples_per_motif': 4,
-            'motif_occurrences': 3,
-        }
+        self.collage = Collage(
+            parent_dir='D:/Samples',
+            sub_dirs=[],
+            main_pool_size=30,
+            sections=('a', 'b'),
+            structure=('a', 'b', 'a', 'b', 'a'),
+            overlap=0.1,
+        )
 
-        self.motif = {
-            'gesture_count': range(2, 10),
-            'fades': (0, 0),
-        }
+        Section = namedtuple('Section',
+                             ('length', 'motif_count', 'samples_per_motif', 'motif_occurrences'))
 
-        self.gesture = {
-            'repeats': range(1, 10),
-            'fades': (0.1, 0.1),
-        }
+        self.section = Section(
+            length=30,  # seconds
+            motif_count=10,
+            samples_per_motif=4,
+            motif_occurrences=3,
+        )
 
-        self.slice = {
-            'length': range(100, 300),
-            'fades': (0.01, 0.01),
-        }
+        Motif = namedtuple('Motif',
+                           ('gesture_count', 'fades'))
 
+        self.motif = Motif(
+            gesture_count=range(2, 10),
+            fades=(0, 0),
+        )
+
+        Gesture = namedtuple('Gesture',
+                             ('repeats', 'fades'))
+
+        self.gesture = Gesture(
+            repeats=range(1, 10),
+            fades=(0.1, 0.1),
+        )
+
+        Slice = namedtuple('Slice',
+                           ('length', 'fades'))
+
+        self.slice = Slice(
+            length=range(100, 300),
+            fades=(0.01, 0.01),
+        )
